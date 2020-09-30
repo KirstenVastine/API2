b1a555f7b89e3f7d260c272f3619b0635aef0610
KirstenVastine <KirstenVastine@gmail.com>
2020-09-29 22:05:05 -0400

    Progress

--- public/script.js
+++ public/script.js
@@ -1,6 +1,18 @@
-const baseURL = "http://api.coinlayer.com/api/"
+const baseURL = "http://api.coinlayer.com/api/live"
 
-function coins(){
-    fetch('http://api.coinlayer.com/api/live?access_key=da13a52f285512b555a4ae6f43eba046');
-    
-}
+async function coins(){
+    await fetch('http://api.coinlayer.com/api/live?access_key=da13a52f285512b555a4ae6f43eba046&symbols=BTC,ETH,XRP')
+    // then(results => console.log(results));
+    .then(results => results.json())
+    .then(json => console.log(json))
+    // console.log(results)
+    .catch(err => console.log(err));
+};
+coins ();
+
+
+
+
+// const button = document.querySelector("button");
+// button.addEventListener('click', results);
+// let container = document.getElementById('pics')
